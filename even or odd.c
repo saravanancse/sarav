c1350507d2838a82734a235b96b67f2e3208a7aa
saravanancse <saravanakumar.ncse@gmail.com>
2016-12-28 09:46:27 +0530

    Add files via upload

---
+++ even or odd.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int n;
+    printf("enter the value of n");
+    scanf("%d",&n);
+    if(n%2==0)
+    {
+        printf("it is even number");
+    }
+    else{
+        printf("it is odd number");
+    }
+    return 0;
+
+}
